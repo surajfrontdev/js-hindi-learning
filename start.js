0af426bc9546c8f3c361386f994c43108d2e0b50
surajfrontdev <136788200+surajfrontdev@users.noreply.github.com>
2025-11-21 10:29:16 +0000

    variable wali files

--- start.js
+++ start.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const userid = 83730292
+let userName = "suraj Verma"
+var EmailId = "suraj@gmail.com"
+
+console.table([userid, userName, EmailId])
+
+/*
+var can be change
+let can be assing change you can assing only once
+const is fixed in every aspect noting can be change
+*/
+
+console.log(userid)
