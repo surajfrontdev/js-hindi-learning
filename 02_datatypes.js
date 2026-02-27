387365c966b8b19975cecd0810c4217fd72ffbb2
surajfrontdev <136788200+surajfrontdev@users.noreply.github.com>
2026-02-27 07:53:43 +0000

    this is about datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,18 @@
+"use_strict";
+
+let name = "suraj"
+let age = 27;
+let isLoggedin = true;
+
+//number = its range 2 to power53 something
+// bigint = bigger then number
+//string = "name etc"
+//boolean = true/false
+//null = null standalone value (representative of empty value) pro tip - its is an object (type0f)
+//undefined = undefined values 
+//symbol = uniqueness
+// object = 
+
+//how to check
+
+console.log(typeof age);
